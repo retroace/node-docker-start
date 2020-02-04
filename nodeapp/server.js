a7efcff1ad96c735c32f79ea218b987467409e58
retroace <rajeshdpaudel@gmail.com>
2020-02-04 23:27:39 +0545

    Added docker-compose file

---
+++ nodeapp/server.js
@@ -0,0 +1,8 @@
+var app = require('express')();
+var http = require('http').createServer(app);
+
+app.get('/', function(req, res){
+  res.send('<h1>Hello world</h1>');
+});
+
+http.listen(process.env.PORT || 3000, console.log('listening on *:3000'));
